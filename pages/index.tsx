547a8ba65e6b558af06dcc1d5b6a4d4fd5aaf1ee
daniela <farias.danicris@gmail.com>
2021-07-10 00:26:15 -0300

    Adição de head e title.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,15 @@
 import Header from '../components/header';
 import Calculator from '../components/calculator';
+import Head from 'next/head';
 
 export default function Home() {
   return (
     <>
+
+      <Head>
+        <title>Calculadora de IMC</title>
+      </Head>
+
       <Header />
       <Calculator />
     </>
